4e3e872937c1478c9bd6c6655bfc3a0136b2495c
Radik Abubjazov <abubjazov@mail.ru>
2021-09-09 19:48:45 +0300

    скрипт customizator: + функция onScaleChange

--- src/js/customizator.js
+++ src/js/customizator.js
@@ -1,12 +1,42 @@
 export default class Customizator {
     constructor() {
-
-    }
-
-    render() {
         this.btnBlock = document.createElement('div');
         this.colorPicker = document.createElement('input');
 
+        this.btnBlock.addEventListener('click', (e) => this.onScaleChange(e));
+    }
+
+    onScaleChange(e) {
+        let scale;
+        const body = document.querySelector('body');
+
+        if (e.target.value) {
+            scale = +e.target.value.replace(/X/g, '');
+        }
+
+        function recursy(element) {
+            element.childNodes.forEach(node => {
+                if (node.nodeName === '#text' && node.nodeValue.replace(/\S+/g, '').length > 0) {
+
+                    if (!node.parentNode.getAttribute('data-fz')) {
+                        let nodeFzStyles = window.getComputedStyle(node.parentNode, null).fontSize;
+
+                        node.parentNode.setAttribute('data-fz', +nodeFzStyles.replace(/px/g, ''));
+                        node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fz') * scale + 'px';
+                    } else {
+                        node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fz') * scale + 'px';
+                    }
+
+                } else {
+                    recursy(node);
+                }
+            });
+        }
+
+        recursy(body);
+    }
+
+    render() {
         let scaleInputS = document.createElement('input'),
             scaleInputM = document.createElement('input'),
             panel = document.createElement('div');
@@ -16,6 +46,14 @@ export default class Customizator {
         scaleInputS.classList.add('scale_btn');
         scaleInputM.classList.add('scale_btn');
         this.btnBlock.classList.add('scale');
+        this.colorPicker.classList.add('color');
+
+        scaleInputS.setAttribute('type', 'button');
+        scaleInputM.setAttribute('type', 'button');
+        scaleInputS.setAttribute('value', '1X');
+        scaleInputM.setAttribute('value', '1.5X');
+        this.colorPicker.setAttribute('type', 'color');
+        this.colorPicker.setAttribute('value', '#ffffff');
 
         this.btnBlock.append(scaleInputS, scaleInputM);
 
