65aa008f51848f9b372357089fc2e8c6b5097573
Radik Abubjazov <abubjazov@mail.ru>
2021-09-09 18:59:04 +0300

    проверка работоспособности сборки для gulp

---
+++ src/js/customizator.js
@@ -0,0 +1,13 @@
+export default class Customizator {
+    constructor() {
+
+    }
+
+    render() {
+        this.btnBlock = document.createElement('div');
+        let scaleInputS = document.createElement('inpit'),
+            scaleInputM = document.createElement('inpit');
+
+        console.log(this.btnBlock, scaleInputS, scaleInputM);
+    }
+}
