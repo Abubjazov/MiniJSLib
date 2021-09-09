bcd612ba82309cfa1d054de3a88a351f6f50b2ac
Radik Abubjazov <abubjazov@mail.ru>
2021-09-09 19:11:05 +0300

    создаём и добавляем на страницу элемент panel

--- src/js/customizator.js
+++ src/js/customizator.js
@@ -5,9 +5,22 @@ export default class Customizator {
 
     render() {
         this.btnBlock = document.createElement('div');
-        let scaleInputS = document.createElement('inpit'),
-            scaleInputM = document.createElement('inpit');
+        this.colorPicker = document.createElement('input');
 
-        console.log(this.btnBlock, scaleInputS, scaleInputM);
+        let scaleInputS = document.createElement('input'),
+            scaleInputM = document.createElement('input'),
+            panel = document.createElement('div');
+
+        panel.append(this.btnBlock, this.colorPicker);
+
+        scaleInputS.classList.add('scale_btn');
+        scaleInputM.classList.add('scale_btn');
+        this.btnBlock.classList.add('scale');
+
+        this.btnBlock.append(scaleInputS, scaleInputM);
+
+        panel.classList.add('panel');
+
+        document.querySelector('body').append(panel);
     }
 }
